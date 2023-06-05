81ffd30d6d844e1d2fe8f3e8b8bb3a90c902fe63
TProgrammer <77713508+TProgramer@users.noreply.github.com>
2023-06-05 22:46:14 +0900

    Init Spring Boot App in VS Code (#2)

---
+++ src/main/java/com/ssacation/ssacation/SsacationApplication.java
@@ -0,0 +1,13 @@
+package com.ssacation.ssacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsacationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssacation/ssacation/SsacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssacation.ssacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsacationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
