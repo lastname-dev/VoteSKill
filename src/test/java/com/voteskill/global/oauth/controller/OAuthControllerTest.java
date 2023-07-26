06347f9560c12c5bcc136c3f4585f6913c513639
chaeyeong222 <iky1505@naver.com>
2023-07-26 12:59:57 +0900

    :twisted_rightwards_arrows: Test : 테스트코드 삭제

--- src/test/java/com/voteskill/global/oauth/controller/OAuthControllerTest.java
+++ src/test/java/com/voteskill/global/oauth/controller/OAuthControllerTest.java
@@ -21,43 +21,43 @@ import static org.mockito.Mockito.when;
 
 public class OAuthControllerTest {
 
-    @Mock
-    private OAuthService oAuthService;
-
-    @InjectMocks
-    private OAuthController oAuthController;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    @DisplayName("인증 코드로 kakao 토큰 받아오기")
-    public void testKakaoCallback() throws JsonProcessingException {
-        String mockCode = "mock_code";
-        String mockAccessToken = "mock_access_token";
-        String mockUserInfo = getMockUserInfo();
-
-        // OAuthService의 getKakaoAccessToken() 메소드가 mockAccessToken을 반환하도록 설정
-        when(oAuthService.getKakaoAccessToken(anyString())).thenReturn(mockAccessToken);
-        // OAuthService의 getUserInfo() 메소드가 mockUserInfo를 반환하도록 설정
-        when(oAuthService.getUserInfo(anyString())).thenReturn(mockUserInfo);
-
-        ResponseEntity<?> responseEntity = oAuthController.kakaoCallback(mockCode);
-
-        // 예상한 결과와 실제 반환값이 일치하는지 확인
-        assertEquals(ResponseEntity.ok("성공~!"), responseEntity);
-    }
-
-    public String getMockUserInfo() throws JsonProcessingException {
-        Map<String, Object> mockUserInfo = new HashMap<>();
-        mockUserInfo.put("nickname", "jm");
-        mockUserInfo.put("email", "jm@naver.com");
-
-        // Jackson ObjectMapper를 사용하여 JSON 형태의 문자열로 변환
-        ObjectMapper objectMapper = new ObjectMapper();
-        String mockUserInfoJson = objectMapper.writeValueAsString(mockUserInfo);
-        return mockUserInfoJson;
-    }
+//    @Mock
+//    private OAuthService oAuthService;
+//
+//    @InjectMocks
+//    private OAuthController oAuthController;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    @DisplayName("인증 코드로 kakao 토큰 받아오기")
+//    public void testKakaoCallback() throws JsonProcessingException {
+//        String mockCode = "mock_code";
+//        String mockAccessToken = "mock_access_token";
+//        String mockUserInfo = getMockUserInfo();
+//
+//        // OAuthService의 getKakaoAccessToken() 메소드가 mockAccessToken을 반환하도록 설정
+//        when(oAuthService.getKakaoAccessToken(anyString())).thenReturn(mockAccessToken);
+//        // OAuthService의 getUserInfo() 메소드가 mockUserInfo를 반환하도록 설정
+//        when(oAuthService.getUserInfo(anyString())).thenReturn(mockUserInfo);
+//
+//        ResponseEntity<?> responseEntity = oAuthController.kakaoCallback(mockCode);
+//
+//        // 예상한 결과와 실제 반환값이 일치하는지 확인
+//        assertEquals(ResponseEntity.ok("성공~!"), responseEntity);
+//    }
+//
+//    public String getMockUserInfo() throws JsonProcessingException {
+//        Map<String, Object> mockUserInfo = new HashMap<>();
+//        mockUserInfo.put("nickname", "jm");
+//        mockUserInfo.put("email", "jm@naver.com");
+//
+//        // Jackson ObjectMapper를 사용하여 JSON 형태의 문자열로 변환
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String mockUserInfoJson = objectMapper.writeValueAsString(mockUserInfo);
+//        return mockUserInfoJson;
+//    }
 }
